885cb1ed7345e784328c940db7729939548bd7b1
Jordan Jonathan Lin <68207907+jardondaful@users.noreply.github.com>
2023-07-20 11:50:57 -0700

    Update main.ts
    appending actually worked here

--- main.ts
+++ main.ts
@@ -1,21 +1,17 @@
 function main(workbook: ExcelScript.Workbook) {
   // Get the source worksheet by name
-  let sourceSheetName = "M2NELab";
-  let destinationSheetName = "Sheet1";
-
-  // Call the function to insert data from source sheet to the end of destination sheet
-  insertSheetDataToEnd(workbook, sourceSheetName, destinationSheetName);
-}
-
-function insertSheetDataToEnd(workbook: ExcelScript.Workbook, sourceSheetName: string, destinationSheetName: string) {
-  // Get the source worksheet by name
-  let sourceSheet = workbook.getWorksheet(sourceSheetName);
+  let sourceSheet = workbook.getWorksheet("M2NELab");
 
   // Get the destination worksheet by name
-  let destinationSheet = workbook.getWorksheet(destinationSheetName);
+  let destinationSheet = workbook.getWorksheet("Sheet1");
 
   // Get the used range of the source sheet
   let sourceRange = sourceSheet.getUsedRange();
+
+  // Output the number of rows and columns in the source range
+  let numRows = sourceRange.getRowCount();
+  let numCols = sourceRange.getColumnCount();
+  console.log("Source Range: " + numRows + " rows, " + numCols + " columns");
 
   // Get the values in the source range
   let values = sourceRange.getValues();
@@ -23,9 +19,12 @@ function insertSheetDataToEnd(workbook: ExcelScript.Workbook, sourceSheetName: s
   // Filter and copy only columns A to J
   let filteredValues = values.map(row => row.slice(0, 10));
 
+  // Output the filtered values array
+  console.log("Filtered Values:");
+  console.log(filteredValues);
+
   // Clear the destination sheet before pasting the data
-  let destinationRange = destinationSheet.getRange("A1:J" + (filteredValues.length));
-  destinationRange.clear();
+  let destinationRange = destinationSheet.getRange("A1:J" + filteredValues.length);
 
   // Set the values in the destination sheet
   destinationRange.setValues(filteredValues);
@@ -33,16 +32,31 @@ function insertSheetDataToEnd(workbook: ExcelScript.Workbook, sourceSheetName: s
   // Get the used range of the destination sheet
   let destinationUsedRange = destinationSheet.getUsedRange();
 
-  // Get the values in the destination used range
-  let destinationValues = destinationUsedRange.getValues();
+  // Get the last row number with text in the destination sheet
+  let lastRowWithText = destinationUsedRange ? destinationUsedRange.getRowCount() : 0;
 
-  // Filter out blank rows
-  let nonBlankValues = destinationValues.filter(row => row.slice(0, 9).some(cellValue => cellValue !== ""));
+  // Now, let's append data from "M2NW Hallway (Front of 60) -->"
+  let sourceSheet2 = workbook.getWorksheet("M2NW Hallway (Front of 60) -->");
 
-  // Clear the destination sheet
-  destinationUsedRange.clear();
+  // Get the used range of the second source sheet
+  let sourceRange2 = sourceSheet2.getUsedRange();
 
-  // Set the non-blank values back to the destination sheet
-  let nonBlankRange = destinationSheet.getRangeByIndexes(0, 0, nonBlankValues.length, nonBlankValues[0].length);
-  nonBlankRange.setValues(nonBlankValues);
+  // Get the values in the second source range
+  let values2 = sourceRange2.getValues();
+
+  // Determine the destination range for the second source data
+  let destinationRange2 = destinationSheet.getRangeByIndexes(lastRowWithText, 0, values2.length, values2[0].length);
+
+  // Set the values in the destination sheet from the second source
+  destinationRange2.setValues(values2);
+
+  // Get the last row number with text in the destination sheet
+  let finalRowWithText = destinationSheet.getUsedRange().getRowCount();
+
+  // Get the last column number with text in the destination sheet
+  let finalColumnWithText = destinationSheet.getUsedRange().getColumnCount();
+
+  // Output the last row and last column with text
+  console.log("Final Row with Text: " + finalRowWithText);
+  console.log("Final Column with Text: " + finalColumnWithText);
 }
