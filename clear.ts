0bbb766d1379c890f4a7bb98e165a4539eb4a876
Jordan Jonathan Lin <68207907+jardondaful@users.noreply.github.com>
2023-07-21 14:49:11 -0700

    Update clear.ts

--- clear.ts
+++ clear.ts
@@ -20,5 +20,27 @@ function main(workbook: ExcelScript.Workbook) {
       // Clear everything within this worksheet
       worksheet.getUsedRange()?.clear();
     }
+
+    // Set the style of the first row
+    let firstRow = worksheet.getRange("1:1");
+    firstRow.getFormat().getFill().setColor("002060"); // dark blue
+    firstRow.getFormat().getFont().setColor("FFFFFF"); // white
+    firstRow.getFormat().getFont().setBold(true); // bold
+    firstRow.getFormat().getFont().setSize(18); // font size 18
+
+    // Get the used range of the worksheet
+    let usedRange = worksheet.getUsedRange();
+
+    // Get the total number of columns
+    let columnCount = usedRange.getColumnCount();
+
+    // Iterate over each column and set its width
+    for (let i = 0; i < columnCount; i++) {
+      // Get range of the current column
+      let columnRange = worksheet.getRangeByIndexes(0, i, usedRange.getRowCount(), 1);
+
+      // Set the width of this column to ~250 pixels (approx 31.25 character units)
+      columnRange.getFormat().setColumnWidth(200);
+    }
   });
 }
